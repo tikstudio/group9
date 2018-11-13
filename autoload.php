86180f722b86cd2aaeec4a881a52fc6db7fbe466
Tigran Muradyan <tigran.inbox@gmail.com>
2018-11-13 21:00:59 +0400

    fixed untracked files

--- autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-function __autoload($class_name) {
-    $file = str_replace('\\', '/', $class_name) . '.php';
-    if (file_exists($file)) {
-        require $file;
-    }
-}
--- view/footer.php
+++
@@ -1,7 +0,0 @@
-</div>
-
-<script src="<?= SITE_URL ?>/assets/scripts/jquery.min.js"></script>
-<script src="<?= SITE_URL ?>/assets/scripts/script.js"></script>
-
-</body>
-</html>
