2d7e0bb8dbb33a68a148478181c6d080e1a45bd3
Tigran Muradyan <tigran.inbox@gmail.com>
2018-11-13 20:41:29 +0400

    first commit

---
+++ model/Register.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace model;
+
+
+class Register extends Model {
+
+
+    public function createUser($email, $name, $pass) {
+        $pass = $this->passHash($pass);
+
+        $registrated_date = date('Y-m-d h:i:s');
+
+        return $this->query("INSERT INTO users(email, user_name, password, registrated_date) 
+                          VALUES (:email, :user_name, :password, :date)",
+            [
+                'email' => $email,
+                'user_name' => $name,
+                'password' => $pass,
+                'date' => $registrated_date,
+            ]);
+    }
+
+    public function getUserByEmail($email) {
+        return $this->getRow("SELECT * from users where email = :email",[
+            'email' => $email
+        ]);
+    }
+
+    public function getUserById($id) {
+        return $this->getRow("select id,user_name from users where id = :id",[
+            'id' => $id
+
+        ]);
+    }
+
+
+}
