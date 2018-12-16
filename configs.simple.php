d0d84eec560cfcc6cc293a87e4ae9efba29c34cf
Tigran Muradyan <tigran.inbox@gmail.com>
2018-12-16 11:23:55 +0400

    added config simple file

---
+++ configs.simple.php
@@ -0,0 +1,16 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'chat');
+
+define('SITE_URL', 'http://localhost');
+define('ROOT_DIR', '/');
+
+define('RECAPTCHA_PUBLIC', '6LfFMIAUAAAAAJVisTmVmKZLHLNiFAC_QWZlssZS');
+define('RECAPTCHA_PRIVATE', '6LfFMIAUAAAAAL4aJrgCa3xLwjJCJRu8ukB0JY6d');
+
+define('FACEBOOK_APP_ID', '1074277512752580');
+define('FACEBOOK_SECRET', '9fc1df05f53c2e00e9ba30a3fb830577');
+
+define('PASS_HASH', '5sfdsf@%^&*45451');
